386178fd642223394f19b538d1aef21291e2b252
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-06-13 21:14:10 -0700

    added connection, schema, and pw files to activate and use mysql

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: myPassword,
-    database: "employees"
+    database: "employeeTable"
 });
 
 connection.connect(function(err){
--- pw.js
+++ pw.js
@@ -1,1 +1,4 @@
 // This file holds the password and username for mySQL workbench, this is .gitignored
+const mySqlPw = "#rootroot";
+
+module.exports = mySqlPw;
